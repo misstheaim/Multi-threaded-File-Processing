3fb273ae288d42fed0f80851709e5c017c3f8e46
alex <alexandr.paramon56@gmail.com>
2025-06-09 18:08:26 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Multi_threaded_File_Processing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
